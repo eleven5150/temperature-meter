8530c874579a36792df311e163cd223ff294b684
Kirill <kirSM2010@gmail.com>
2023-12-06 00:45:16 +0300

    Updated ring buffer to new version

--- Core/Inc/uart_ringbuffer.h
+++ Core/Inc/uart_ringbuffer.h
@@ -6,11 +6,10 @@
 /* change the size of the buffer */
 #define UART_BUFFER_SIZE 64
 
-typedef struct
-{
-  unsigned char buffer[UART_BUFFER_SIZE];
-  volatile unsigned int head;
-  volatile unsigned int tail;
+typedef struct {
+    unsigned char buffer[UART_BUFFER_SIZE];
+    volatile unsigned int head;
+    volatile unsigned int tail;
 } ring_buffer;
 
 
@@ -28,8 +27,7 @@ void Uart_sendstring(const char *s);
 
 /* Print a number with any base
  * base can be 10, 8 etc*/
-void Uart_printbase (long n, uint8_t base);
-
+void Uart_printbase(long n, uint8_t base);
 
 
 /* Checks if the data is available to read in the rx_buffer */
@@ -40,18 +38,18 @@ int IsDataAvailable(void);
  * @return 1, if the string is found and -1 if not found
  * @USAGE:: if (Look_for ("some string", buffer)) do something
  */
-int Look_for (char *str, char *buffertolookinto);
+int Look_for(char *str, char *buffertolookinto);
 
 /* Copies the required data from a buffer
  * @startString: the string after which the data need to be copied
  * @endString: the string before which the data need to be copied
  * @USAGE:: GetDataFromBuffer ("name=", "&", buffertocopyfrom, buffertocopyinto);
  */
-void GetDataFromBuffer (char *startString, char *endString, char *buffertocopyfrom, char *buffertocopyinto);
+void GetDataFromBuffer(char *startString, char *endString, char *buffertocopyfrom, char *buffertocopyinto);
 
 
 /* Resets the entire ring buffer, the new data will start from position 0 */
-void Uart_flush (void);
+void Uart_flush(void);
 
 /* Peek for the data in the Rx Bffer without incrementing the tail count 
 * Returns the character
@@ -65,25 +63,25 @@ int Uart_peek();
 * Returns 1 on success and -1 otherwise
 * USAGE: while (!(Copy_Upto ("some string", buffer)));
 */
-int Copy_upto (char *string, char *buffertocopyinto);
+int Copy_upto(char *string, char *buffertocopyinto);
 
 
 /* Copies the entered number of characters (blocking mode) from the Rx buffer into the buffer, after some particular string is detected
 * Returns 1 on success and -1 otherwise
 * USAGE: while (!(Get_after ("some string", 6, buffer)));
 */
-int Get_after (char *string, uint8_t numberofchars, char *buffertosave);
+int Get_after(char *string, uint8_t numberofchars, char *buffertosave);
 
 
 /* Wait until a paricular string is detected in the Rx Buffer
 * Return 1 on success and -1 otherwise
 * USAGE: while (!(Wait_for("some string")));
 */
-int Wait_for (char *string);
+int Wait_for(char *string);
 
 
 /* the ISR for the uart. put it in the IRQ handler */
-void Uart_isr (UART_HandleTypeDef *huart);
+void Uart_isr(UART_HandleTypeDef *huart);
 
 
 
